7cb90fcb1d326880336e6c314d31aeab3c534b82
yunaa <yuna.ads@hotmail.com>
2024-02-07 20:04:54 -0300

    Banner

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-    <p>Footer</p>
+   <footer id="footer">
+    <p>Make you Burger &copy; 2024</p>
+
+   </footer>
 </template>
 
 <script>
@@ -7,3 +10,18 @@ export default {
     name: "Footer"
 }
 </script>
+
+<style scoped>
+
+ #footer {
+    height: 310px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+ }
+  
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="nav">
-        <router-link to="/">
-            <img src="" id="logo"/>
+        <router-link to="/" id="logo-url">
+            <img :src="logo" id="logo" :alt="alt"/>
         </router-link>
-        <router-link to="/">Home</router-link> |
+        <router-link to="/">Home</router-link> 
         <router-link to="/pedidos">Pedidos</router-link>
     </div>
      
@@ -12,11 +12,47 @@
 
 <script>
 export default {
-    name: "Navbar"
+    name: "Navbar",
+    props: ["logo", "alt"]
 }
 </script>
 
 
 <style scoped>
+  #nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+  }
+
+  #nav a {
+    color: #FCBA03;
+    text-decoration: none;
+    margin: 12px;
+    transition: 0.5s;
+  }
+
+  #nav a:hover {
+    color: #fff;
+  }
+
+  #nav #logo-url {
+    margin: auto;
+    margin-left: 0;
+
+  }
+
+  #logo {
+    width: 40px;
+    height: 40px;
+  }
+
+ 
+
+
 
 </style>
